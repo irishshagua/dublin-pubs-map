886e2ad9dac331b5748b9363b10850339f03fb96
bmooney <brian.mooney@paddypower.com>
2016-10-06 10:05:35 +0100

    Coords should be Number. Also add Dublin boundary...

--- convertToGeoJson.py
+++ convertToGeoJson.py
@@ -4,30 +4,34 @@
 import csv
 import json
 
-with open("postgres-pubs-dump.txt") as file: 
-    reader = csv.reader(file, delimiter='\t')
+with open("dublinBoundary.geojson") as dublinPolygonJsonFile:
+	dublinPolygon = json.load(dublinPolygonJsonFile)
 
-    data = {}
-    data['type'] = 'FeatureCollection'
-    features = []
-    for row in reader:
-    	feature = {}
-    	feature['type'] = 'Feature'
-    	
-    	geometry = {}
-    	geometry['type'] = 'Point'
-    	geometry['coordinates'] = [row[1], row[0]]
-    	feature['geometry'] = geometry
-    	
-    	properties = {}
-        properties['marker-symbol'] = 'bar'
-    	properties['name'] = row[2]
-        properties['review'] = row[3]
-    	feature['properties'] = properties
+	with open("postgres-pubs-dump.txt") as file: 
+	    reader = csv.reader(file, delimiter='\t')
 
-    	features.append(feature)
+	    data = {}
+	    data['type'] = 'FeatureCollection'
+	    features = []
+	    for row in reader:
+	    	feature = {}
+	    	feature['type'] = 'Feature'
+	    	
+	    	geometry = {}
+	    	geometry['type'] = 'Point'
+	    	geometry['coordinates'] = [float(row[1]), float(row[0])]
+	    	feature['geometry'] = geometry
+	    	
+	    	properties = {}
+	        properties['marker-symbol'] = 'beer'
+	    	properties['name'] = row[2]
+	        properties['review'] = row[3]
+	    	feature['properties'] = properties
 
-    data['features'] = features
+	    	features.append(feature)
 
-    with open('dublin-pubs.geojson', 'w') as outfile:
-	    json.dump(data, outfile)
+	    features.append(dublinPolygon)
+	    data['features'] = features
+
+	    with open('dublin-pubs.geojson', 'w') as outfile:
+		    json.dump(data, outfile, indent = 2)
